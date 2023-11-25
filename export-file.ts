2916c252f87f075109580a03bf2cab0b82195d16
D.Yogesh <yogeshone678@gmail.com>
2023-11-25 06:19:53 -0800

    add: v2-ledger
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

---
+++ export-file.ts
@@ -0,0 +1,12 @@
+export {
+    DEFAULT_FABRIC_2_AIO_FABRIC_VERSION_V2,
+    DEFAULT_FABRIC_2_AIO_IMAGE_NAME_V2,
+    DEFAULT_FABRIC_2_AIO_IMAGE_VERSION_V2,
+    FabricTestLedgerV2,
+    IFabricTestLedgerV2ConstructorOptions,
+    FABRIC_TEST_LEDGER_DEFAULT_OPTIONS_V2,
+    FABRIC_TEST_LEDGER_OPTIONS_JOI_SCHEMA_V2,
+    STATE_DATABASE_V2,
+    organizationDefinitionFabricV2_V2,
+    LedgerStartOptions_V2,
+  } from "./fabric/fabric-test-ledger-v2";
